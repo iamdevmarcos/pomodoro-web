e297e177b9cb344bb643edf98ae111a4438892af
Marcos Andre <marcosdev.me@gmail.com>
2022-04-30 10:20:38 -0300

    feat: add starting working button

--- src/components/organisms/PomodoroTimer/index.tsx
+++ src/components/organisms/PomodoroTimer/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "./styles";
 
 import { PomodoroTimerProps } from "../../../interfaces/pomodoro";
@@ -9,10 +9,26 @@ import { Timer, Button } from "../../index";
 
 const PomodoroTimer: React.FC<PomodoroTimerProps> = ({ defaultTime }) => {
   const [mainTime, setMainTime] = useState(defaultTime);
+  const [timeCounter, setTimeCounter] = useState(false);
+  const [working, setWorking] = useState(false);
 
-  useInterval(() => {
-    setMainTime((prevState) => prevState - 1);
-  }, 1000);
+  useInterval(
+    () => {
+      setMainTime((prevState) => prevState - 1);
+    },
+    timeCounter ? 1000 : null
+  );
+
+  const startWorking = () => {
+    setTimeCounter(true);
+    setWorking(true);
+  };
+
+  useEffect(() => {
+    if (working) {
+      document.body.classList.add("working");
+    }
+  }, [working]);
 
   return (
     <Container>
@@ -21,7 +37,7 @@ const PomodoroTimer: React.FC<PomodoroTimerProps> = ({ defaultTime }) => {
         <Timer mainTime={mainTime} />
 
         <div className="controls">
-          <Button title="teste" onClick={() => alert("Starting...")} />
+          <Button title="Work" onClick={startWorking} />
           <Button title="teste" onClick={() => alert("Starting...")} />
           <Button title="teste" onClick={() => alert("Starting...")} />
         </div>
