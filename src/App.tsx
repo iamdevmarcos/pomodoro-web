2c5de7357c4dc26be819696794b777acf57921d0
Marcos Andre <marcosdev.me@gmail.com>
2022-05-01 17:24:14 -0300

    feat: adding github corner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from "react";
-
 import GlobalStyles from "./styles/GlobalStyles";
+
 import { Home } from "./pages";
 import { TopBar } from "./components";
+import GithubCorner from "react-github-corner";
 
 const App: React.FC = () => {
   return (
     <>
-      <TopBar />
-
       <GlobalStyles />
+
+      <TopBar />
+      <GithubCorner
+        href="https://github.com/iamdevmarcos/pomodoro-web"
+        octoColor="#fff"
+        bannerColor="#1c2c4299"
+        target="_blank"
+      />
       <Home />
     </>
   );
