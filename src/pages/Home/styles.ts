0ff331bcd18ceccdca76d9f60b3a3dfde81fce70
Marcos Andre <marcosdev.me@gmail.com>
2022-05-01 09:49:11 -0300

    chore: add image background

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,4 +3,9 @@ import styled from "styled-components";
 export const Container = styled.main`
   max-width: 640px;
   margin: 0 auto;
+
+  background: url("/bg.jpg");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
